4bcb1f2b754fd9b7ee36b52eb1a809bc89958ce0
Bob_work <musgravehill@gmail.com>
2019-04-20 09:20:00 +0300

    g

--- ADF4351/FW/i2cLCD/i2cLCD.ino
+++ ADF4351/FW/i2cLCD/i2cLCD.ino
@@ -7,19 +7,31 @@
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-void setup()
-{
-  lcd.init();                      // initialize the lcd
+void setup() {
+  uint32_t ADF4351_frequency = 243300000L; //*10 Hz = 2.433 GHz
+  uint16_t f_m = ADF4351_frequency / 100000L; //*10Hz
+  uint32_t f_k =  ADF4351_frequency % 100000; //*10Hz
+
+  lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print((float)4444.004, 3);
-  lcd.print(" kHz");
-  lcd.print(" LN");
+  lcd.print(f_m);
+  lcd.print(".");
+  lcd.print(f_k,5);
+  lcd.print(" MHz");
+  lcd.setCursor(0, 1);
+  lcd.print("6.25kHz");
+  lcd.print(" LN -4dBm");
 
-
-  lcd.setCursor(0, 1);  
-  lcd.print("6.25 kHz");  
-  lcd.print(" -4dBm");
+  /*lcd.init();                      // initialize the lcd
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print((float)12000.004, 3);
+    lcd.print("kHz");
+    lcd.print(" SIN");
+    lcd.setCursor(0, 1);
+    lcd.print("6.25kHz");
+    lcd.print("  amp256");*/
 
 }
 
