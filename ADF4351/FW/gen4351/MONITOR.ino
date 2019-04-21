eb36ea18b7676c226cfe50c01321078ed6dd0fd7
Bob_work <musgravehill@gmail.com>
2019-04-21 23:11:22 +0300

    h

--- ADF4351/FW/gen4351/MONITOR.ino
+++ ADF4351/FW/gen4351/MONITOR.ino
@@ -15,34 +15,34 @@ void MONITOR_render() {
   lcd.setCursor(0, 0);
 
   if (frq_MHz < 1000) {
-    lcd.print(0);
+    lcd.print(' ');
   }
   if (frq_MHz < 100) {
-    lcd.print(0);
+    lcd.print(' ');
   }
   if (frq_MHz < 10) {
-    lcd.print(0);
+    lcd.print(' ');
   }
   lcd.print(frq_MHz);
   lcd.print(".");
   
   if (frq_kHz < 100) {
-    lcd.print(0);
+    lcd.print(' ');
   }
   if (frq_kHz < 10) {
-    lcd.print(0);
+    lcd.print(' ');
   }
   lcd.print(frq_kHz);
   lcd.print(".");
    
   if (frq_Hz < 100) {
-    lcd.print(0);
+    lcd.print(' ');
   }
   if (frq_Hz < 10) {
-    lcd.print(0);
+    lcd.print(' ');
   }
   lcd.print(frq_Hz);
-  lcd.print("Hz");
+  lcd.print(" Hz");
 
   lcd.setCursor(0, 1);
   lcd.print(OLED_stepsVariants_val[ADF4351_stepsVariantsNumCurrent]);
