aaad28d59c61cbf49cb5b6b4b887ba612346719e
Bob_work <musgravehill@gmail.com>
2019-04-21 23:09:43 +0300

    m

--- ADF4351/FW/gen4351/MONITOR.ino
+++ ADF4351/FW/gen4351/MONITOR.ino
@@ -6,19 +6,48 @@ void MONITOR_init() {
 
 
 void MONITOR_render() {
+  uint32_t frq_MHz = ADF4351_frequency / 100000;
+  uint32_t frq_kHz = (ADF4351_frequency % 100000) / 100;
+  uint32_t frq_Hz = (ADF4351_frequency % 100) * 10;
+
+
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print((int)(ADF4351_frequency/100000));
+
+  if (frq_MHz < 1000) {
+    lcd.print(0);
+  }
+  if (frq_MHz < 100) {
+    lcd.print(0);
+  }
+  if (frq_MHz < 10) {
+    lcd.print(0);
+  }
+  lcd.print(frq_MHz);
   lcd.print(".");
-  lcd.print((ADF4351_frequency%100000)/100);
+  
+  if (frq_kHz < 100) {
+    lcd.print(0);
+  }
+  if (frq_kHz < 10) {
+    lcd.print(0);
+  }
+  lcd.print(frq_kHz);
   lcd.print(".");
-  lcd.print((ADF4351_frequency%100)*10);  
-  lcd.print("Hz");  
+   
+  if (frq_Hz < 100) {
+    lcd.print(0);
+  }
+  if (frq_Hz < 10) {
+    lcd.print(0);
+  }
+  lcd.print(frq_Hz);
+  lcd.print("Hz");
 
   lcd.setCursor(0, 1);
   lcd.print(OLED_stepsVariants_val[ADF4351_stepsVariantsNumCurrent]);
   lcd.print(' ');
   lcd.print(ADF4351_lowNoiseOrSpur_verb[ADF4351_lowNoiseOrSpur_current]);
   lcd.print(' ');
-  lcd.print(ADF4351_outputPower_verb[ADF4351_outputPower_current]);  
+  lcd.print(ADF4351_outputPower_verb[ADF4351_outputPower_current]);
 }
