561bf6c1fd0efec3b17aa851b31eb7eeadacb3f6
Bob_work <musgravehill@gmail.com>
2021-02-19 10:41:30 +0300

    gen

--- MAX2870-MAX2871-6GHZ/FW/v1/TIMEMACHINE.ino
+++ MAX2870-MAX2871-6GHZ/FW/v1/TIMEMACHINE.ino
@@ -21,7 +21,7 @@ void TIMEMACHINE_101ms() {
 void TIMEMACHINE_311ms() {
   BUTTON_check();  
   SERIAL_processData();
-  //MONITOR_render(); // для теста!!! Правильно MONITOR_render только на событиях после кнопки и т.д. 
+  //MONITOR_render(); // для теста!!! Правильно MONITOR_render только на событиях SYS_events_check() 
 }
 
 void TIMEMACHINE_2000ms() {
